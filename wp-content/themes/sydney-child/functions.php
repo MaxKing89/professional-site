5219d4256e96b49405735008ac89daac576c103c
Max <robert.max.king@gmail.com>
2017-12-13 14:28:16 -0500

    google analytics addition

--- wp-content/themes/sydney-child/functions.php
+++ wp-content/themes/sydney-child/functions.php
@@ -5,6 +5,9 @@ if ( !defined( 'ABSPATH' ) ) exit;
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
+
+
+
 if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
     function chld_thm_cfg_parent_css() {
         wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'sydney-bootstrap' ) );
